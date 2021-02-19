1f57e3896f3a445739aadd6247b64467f8c06909
Jeroen Brauns <brauns.jeroen@pm.me>
2021-02-19 20:31:49 +0100

    [new] function main

--- src/main.py
+++ src/main.py
@@ -10,7 +10,23 @@ def stealth_scan():
 	print(scanner.scaninfo())
 
 
-print(scanner.nmap_version())
+# print(scanner.nmap_version())
 
-stealth_scan()
-stealth_scan()
+
+# scanner.scan('127.0.0.1', '21')
+scanner.scan('192.168.0.202', ports="80, 443")
+
+# print(type(scanner))
+
+print(scanner.command_line())
+
+print(scanner.scaninfo())
+
+print(scanner.all_hosts())
+
+print(scanner['127.0.0.1'].state())
+# print(scanner['127.0.0.1'].all_protocols())
+# print(scanner['127.0.0.1']['tcp'].keys())
+# print(scanner['127.0.0.1'].has_tcp(22))
+# print(scanner['127.0.0.1'].has_tcp(80))
+# print(scanner['127.0.0.1'].has_tcp(22))
