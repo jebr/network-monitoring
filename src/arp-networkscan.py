67124137065e36fb38ce5b37e2b7f2c857d30c16
Jeroen Brauns <brauns.jeroen@pm.me>
2021-02-23 21:04:39 +0100

    [new] networkscan scapy

--- src/arp-networkscan.py
+++ src/arp-networkscan.py
@@ -1,5 +1,34 @@
 # Python script to detect devices in network using Scapy
 
-from scapy.all import arping
+from scapy.all import ARP, Ether, srp
+from tabulate import tabulate
 
-arping("192.168.1.0/24")
+# IP Address for the destination
+target_ip = "192.168.0.0/24"
+
+# create ARP packet
+arp = ARP(pdst=target_ip)
+
+# create the Ether broadcast packet
+# ff:ff:ff:ff:ff:ff MAC address indicates broadcasting
+ether = Ether(dst="ff:ff:ff:ff:ff:ff")
+
+# stack them
+packet = ether/arp
+
+result = srp(packet, timeout=3, verbose=0)[0]
+
+# a list of clients, we will fill this in the upcoming loop
+clients = []
+
+for sent, received in result:
+    # for each response, append ip and mac address to `clients` list
+    clients.append({'IP-address': received.psrc, 'MAC-address': received.hwsrc})
+
+# print clients
+print("Available devices in the network:")
+print(tabulate(clients, headers="keys"))
+
+# print("IP" + " "*18+"MAC")
+# for client in clients:
+#     print("{:16}    {}".format(client['ip'], client['mac']))
